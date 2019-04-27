fb49041d82d4a31c0d34f0bbc08089fd70f4e18a
GamingNight <34170706+GamingNight@users.noreply.github.com>
2019-04-27 11:13:40 +0200

    first scene

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject objectToFollow;
+
+    void Update() {
+
+        transform.position = new Vector3(objectToFollow.transform.position.x, transform.position.y, objectToFollow.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    public float strength = 1000;
+    public float rotateSpeed = 250;
+    Rigidbody rgbd;
+
+    private void Start() {
+
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        bool accelerate = Input.GetButton("Accelerate");
+
+        if (accelerate) {
+            rgbd.AddRelativeForce(Time.deltaTime * strength, h * Time.deltaTime * strength, 0);
+            transform.Rotate(0,0,-h * Time.deltaTime * rotateSpeed, Space.Self);
+        }
+    }
+}
