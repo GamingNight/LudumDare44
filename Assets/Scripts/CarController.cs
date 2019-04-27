c84cea61acbce3684c83219e6e5412a310f7b29a
JFAURE <julien.y.faure@gmail.com>
2019-04-27 17:00:22 +0200

    speed drift

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,26 +1,37 @@
 ﻿using UnityEngine;
 
-public class CarController : MonoBehaviour {
+public class CarController : MonoBehaviour
+{
     public float carAcceleration = 1000;
     public float brakingInit = 50;
     public float steering = 0.2f;
     public float timer = 0f;
     float braking;
     Rigidbody rgbd;
+    private ParticleSystem[] pcSystems;
 
-    private void Start() {
+    private void Start()
+    {
 
         rgbd = GetComponent<Rigidbody>();
+        pcSystems = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem pcSystem in pcSystems)
+        {
+            //pcSystem.Pause(true);
+        }
+
         braking = brakingInit;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         bool accelerate = Input.GetButton("Accelerate");
         float speedLocal = transform.InverseTransformDirection(rgbd.velocity).x;
         float speedDrift = transform.InverseTransformDirection(rgbd.velocity).y;
-        if (speedLocal <= 1){
+        if (speedLocal <= 1)
+        {
             timer = timer + Time.deltaTime;
         }
         else
@@ -29,30 +40,40 @@ public class CarController : MonoBehaviour {
         }
         if (v < 0)
         {
-            braking = braking + Time.deltaTime*brakingInit/braking*40;
+            braking = braking + Time.deltaTime * brakingInit / braking * 40;
             if (timer > 0.1)
             {
                 rgbd.AddRelativeForce(-Time.deltaTime * carAcceleration / 2, 0, 0);
             }
             else
             {
-                rgbd.AddRelativeForce(-Time.deltaTime * carAcceleration * braking / brakingInit*speedLocal/40, 0, 0);
+                rgbd.AddRelativeForce(-Time.deltaTime * carAcceleration * braking / brakingInit * speedLocal / 40, 0, 0);
             }
         }
         else
         {
             braking = brakingInit;
-
         }
-        Debug.Log(braking);
-        if (accelerate) {
+        if (accelerate)
+        {
             rgbd.AddRelativeForce(Time.deltaTime * carAcceleration, 0, 0);
         }
 
-//        rgbd.velocity = transform.TransformDirection(new Vector3(10, 0, 0));
- //       Debug.Log(rgbd.velocity);
-        rgbd.AddRelativeForce(0, -Time.deltaTime * braking *  speedDrift, 0);
-        transform.Rotate(0, 0, -h*Time.deltaTime * speedLocal*steering * braking);
-
+        //        rgbd.velocity = transform.TransformDirection(new Vector3(10, 0, 0));
+        //       Debug.Log(rgbd.velocity);
+        rgbd.AddRelativeForce(0, -Time.deltaTime * braking * speedDrift, 0);
+        transform.Rotate(0, 0, -h * Time.deltaTime * speedLocal * steering * braking);
+        foreach (ParticleSystem pcSystem in pcSystems)
+        {
+            if (Mathf.Abs(speedDrift) > 10 && !pcSystem.isPlaying)
+            {
+                pcSystem.Play(true);
+            }
+            else if (Mathf.Abs(speedDrift) < 9)
+            {
+                pcSystem.Pause(true);
+            }
+            //Debug.Log(pcSystem.isPlaying+"    "+speedDrift);
+        }
     }
 }
