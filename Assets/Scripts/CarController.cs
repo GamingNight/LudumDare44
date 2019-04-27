5c8f2879ac9f59b1fc91ff5653adb81e8ded30a5
JFAURE <julien.y.faure@gmail.com>
2019-04-27 14:32:00 +0200

    CarPhysics

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 
 public class CarController : MonoBehaviour {
-    public float strength = 1000;
-    public float rotateSpeed = 250;
+    public float carAcceleration = 1000;
+    public float brakingInit = 10;
+    public float steering = 10;
     Rigidbody rgbd;
 
     private void Start() {
@@ -13,15 +14,29 @@ public class CarController : MonoBehaviour {
     void FixedUpdate() {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
+        float braking = brakingInit;
         bool accelerate = Input.GetButton("Accelerate");
+        float speedLocal = transform.InverseTransformDirection(rgbd.velocity).x;
+        float speedDrift = transform.InverseTransformDirection(rgbd.velocity).y;
+        if (v < 0)
+        {
+            braking = braking * 1.2f;
+            rgbd.AddRelativeForce(-Time.deltaTime * carAcceleration/2*braking/brakingInit, 0, 0);
+        }
+        else
+        {
+            braking = brakingInit;
+
+        }
 
         if (accelerate) {
-            rgbd.AddRelativeForce(Time.deltaTime * strength, h * Time.deltaTime * strength, 0);
+            rgbd.AddRelativeForce(Time.deltaTime * carAcceleration, 0, 0);
         }
 
-        if (Mathf.Abs(rgbd.velocity.x) > 0.01) {
-            transform.Rotate(0, 0, -h * Time.deltaTime * rotateSpeed, Space.Self);
-        }
+//        rgbd.velocity = transform.TransformDirection(new Vector3(10, 0, 0));
+ //       Debug.Log(rgbd.velocity);
+        rgbd.AddRelativeForce(0, -Time.deltaTime * braking *  speedDrift, 0);
+        transform.Rotate(0, 0, -h*Time.deltaTime * speedLocal*steering * braking);
+
     }
 }
