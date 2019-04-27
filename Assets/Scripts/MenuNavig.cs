2054b26e899d80effb5378ef5862152f225be152
JFAURE <julien.y.faure@gmail.com>
2019-04-27 21:49:08 +0200

    menu du début

---
+++ Assets/Scripts/MenuNavig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavig : MonoBehaviour
+{
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(Input.mousePosition);
+        bool submit = Input.GetButtonDown("Submit");
+        Debug.Log(screenPos);
+        if (submit)
+        {
+            Play();
+            Debug.Log("pommier");
+        }
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("SceneFaure");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
