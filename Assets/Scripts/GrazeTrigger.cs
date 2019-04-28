8fc78db5709ba791b5504b50cd43183e6b925139
GamingNight <34170706+GamingNight@users.noreply.github.com>
2019-04-28 23:09:32 +0200

    GrazeTrigger commit conflict + prefab

--- Assets/Scripts/GrazeTrigger.cs
+++ Assets/Scripts/GrazeTrigger.cs
@@ -5,9 +5,33 @@ public class GrazeTrigger : MonoBehaviour {
     public int points = 30;
     public bool active = true;
     public float grazeSpeedLimit = 15;
+    public bool updateViewersAtStay = false;
+
+    private float timeSinceLastStayUpdate;
+
+    private void Start() {
+        timeSinceLastStayUpdate = 0f;
+    }
 
     private void OnTriggerEnter(Collider other) {
 
+        GameObject player = GameManager.Instance().GetPlayer();
+
+        if (other.gameObject == player && active) {
+            CarController carController = player.GetComponent<CarController>();
+            float speedDrift = Mathf.Abs(carController.speedDrift);
+            float speedLocal = Mathf.Abs(carController.speedLocal);
+            if ((speedDrift > grazeSpeedLimit) || (speedLocal > grazeSpeedLimit)) {
+                player.GetComponent<StreamViewManager>().UpdateStreamPoints(points);
+                carController.setFearStatus(true);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        if (!updateViewersAtStay || (Time.time - timeSinceLastStayUpdate) < 0.2)
+            return;
         GameObject player = GameManager.Instance().GetPlayer();
 
         if (other.gameObject == player && active) {
@@ -16,7 +40,7 @@ public class GrazeTrigger : MonoBehaviour {
             float speedLocal = Mathf.Abs(car_controller.speedLocal);
             if ((speedDrift > grazeSpeedLimit) || (speedLocal > grazeSpeedLimit)) {
                 player.GetComponent<StreamViewManager>().UpdateStreamPoints(points);
-                car_controller.setFearStatus(true);
+                timeSinceLastStayUpdate = Time.time;
             }
         }
     }
