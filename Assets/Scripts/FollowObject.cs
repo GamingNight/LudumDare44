3ad2b035cef29ce4d9c2668ab318202156609bd6
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2019-04-28 03:56:47 +0200

    nique ta mere la chienne

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -1,11 +1,39 @@
 ﻿using UnityEngine;
 
-public class FollowObject : MonoBehaviour {
+public class FollowObject : MonoBehaviour
+{
 
     public GameObject objectToFollow;
 
-    void Update() {
+    //void Update() {
 
-        transform.position = new Vector3(objectToFollow.transform.position.x, transform.position.y, objectToFollow.transform.position.z);
+    //    transform.position = new Vector3(objectToFollow.position.x, transform.position.y, objectToFollow.position.z);
+    //}
+
+    public float smooth;
+    public float distance;
+    public float offsetValue;
+
+    private Vector3 position;
+    private Vector3 offset;
+
+    void Start()
+    {
+
+        position = transform.position;
+        offset = new Vector3(offsetValue, 0f, 0f);
+
     }
+
+    void FixedUpdate()
+    {
+
+        position = Vector3.Lerp(position, objectToFollow.transform.position, (smooth * Time.fixedDeltaTime));
+        position = new Vector3(position.x, distance, position.z);
+
+        transform.position = position + offset;
+
+    }
+
+
 }
