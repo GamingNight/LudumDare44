0d5e12a3b4f31b1585de77a320c2712c1c59ddd2
JFAURE <julien.y.faure@gmail.com>
2019-04-28 13:59:24 +0200

    commit menu

--- Assets/Scripts/MenuNavig.cs
+++ Assets/Scripts/MenuNavig.cs
@@ -5,14 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuNavig : MonoBehaviour
 {
-
     // Update is called once per frame
     void Update()
     {
     }
     public void Play()
     {
-        Debug.Log("pommier");
         SceneManager.LoadScene("sampleScene");
     }
     public void Quit()
@@ -22,5 +20,28 @@ public class MenuNavig : MonoBehaviour
     public void startMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+
+    public void InitLaunchMainScene()
+    {
+        StartCoroutine(LaunchMainSceneCoroutine());
+    }
+
+    private IEnumerator LaunchMainSceneCoroutine()
+    {
+        Vector2 targetPosition = new Vector2(900, -1200);
+        float totalTime =0.3f;
+        float step = 0.009f;
+        float currentTime = 0f;
+        while (currentTime < totalTime)
+        {
+            float x = ((RectTransform)transform).anchoredPosition.x;
+            float y = ((RectTransform)transform).anchoredPosition.y;
+            Vector2 velocityDamp = Vector2.zero;
+            Vector2 moveDamp = Vector2.SmoothDamp(((RectTransform)transform).anchoredPosition, targetPosition, ref velocityDamp, 0.03f, Mathf.Infinity,step);
+            ((RectTransform)transform).anchoredPosition = moveDamp;
+            yield return new WaitForSeconds(step);
+            currentTime += step;
+        }
     }
 }
