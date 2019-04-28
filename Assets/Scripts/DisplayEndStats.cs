77f7bc584d1d2e2f45da5f69403a21e05ac8fd84
JFAURE <julien.y.faure@gmail.com>
2019-04-29 01:55:51 +0200

    endmenu

--- Assets/Scripts/DisplayEndStats.cs
+++ Assets/Scripts/DisplayEndStats.cs
@@ -10,9 +10,12 @@ public class DisplayEndStats : MonoBehaviour
 
     void Start()
     {
-        moneyText.text = GameManager.FINAL_MONEY.ToString() + "$";
+        int millions = Mathf.RoundToInt(GameManager.FINAL_MONEY / 1000000);
+        int thousands = Mathf.RoundToInt((GameManager.FINAL_MONEY % 1000000)/1000);
+        int units = Mathf.RoundToInt(GameManager.TOTAL_PLAY_TIME % 1000);
+        moneyText.text = "$ "+millions+"."+ thousands.ToString("D3") +"."+ units.ToString("D3");
         int minutes = Mathf.RoundToInt(GameManager.TOTAL_PLAY_TIME / 60f);
         int seconds = Mathf.RoundToInt(GameManager.TOTAL_PLAY_TIME % 60);
-        playtimeText.text = "The last " + minutes + " minute" + (minutes > 1 ? "s " : " ") + seconds + " second" + (minutes > 1 ? "s " : " ") + "of your life brought you";
+        playtimeText.text = "The last " + minutes + "m " + seconds + "s " + "of your life brought you";
     }
 }
