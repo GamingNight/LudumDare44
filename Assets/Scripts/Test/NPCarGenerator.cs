38c9e7f71430ac887a453e52ef9667ced5a7c692
vilboot <vilboot@hotmail.com>
2019-04-29 19:05:03 +0200

    add car mesh agent scripts

---
+++ Assets/Scripts/Test/NPCarGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class NPCarGenerator : MonoBehaviour {
+
+    public GameObject nPCarPrefab;
+    public GameObject nPCarNavPointContainer;
+
+    private Vector3[] nPCarNavPointsPositions;
+    private GameObject[] nPCars;
+
+    void Start() {
+    	Debug.Log("Nb point"+nPCarNavPointContainer.transform.childCount);
+    	int nbCars = nPCarNavPointContainer.transform.childCount;
+        nPCarNavPointsPositions = new Vector3[nbCars];
+        nPCars = new GameObject[nbCars];
+        for (int i = 0; i < nbCars; i++) {
+        	Debug.Log("i "+ i);
+            nPCarNavPointsPositions[i] = nPCarNavPointContainer.transform.GetChild(i).position;
+            nPCars[i] = Instantiate<GameObject>(nPCarPrefab);
+            nPCars[i].transform.position = nPCarNavPointsPositions[i];
+            nPCars[i].GetComponent<NPCarNavigator>().targetContainer = nPCarNavPointContainer;
+            nPCars[i].GetComponent<NPCarNavigator>().posID = i;
+        }
+    }
+
+    public Vector3[] getNPCarNavPointsPositions() {
+    	return nPCarNavPointsPositions;
+    }
+
+    public GameObject[] getNPCars() {
+    	return nPCars;
+    }
+
+/*    void Update() {
+        currentNPCCount = transform.childCount;
+        if (currentNPCCount < totalNPCCount) {
+            for (int i = 0; i < totalNPCCount - currentNPCCount; i++) {
+                GameObject npcClone = Instantiate<GameObject>(npcPrefab);
+                npcClone.transform.position = npcNavPointsPositions[Random.Range(0, npcNavPointsPositions.Length)];
+                npcClone.transform.parent = transform;
+                npcClone.GetComponent<NPCNavigation>().targetContainer = npcNavPointContainer;
+            }
+        }
+    }*/
+}
---
+++ Assets/Scripts/Test/NPCarNavigator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCarNavigator : MonoBehaviour
+{
+    public GameObject targetContainer;
+    public bool fearStatus = false;
+    private List<Vector3> targets;
+    private Vector3 currentTarget;
+    //private NavMeshAgent agent;
+    private int nbPoints;
+    private int currentID;
+    public int posID = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        //agent = GetComponent<NavMeshAgent>();
+        targets = new List<Vector3>();
+        foreach (Transform t in targetContainer.transform) {
+            targets.Add(t.transform.position);
+        }
+        int nbPoints = targetContainer.transform.childCount;
+        UpdateTarget();
+    }
+        
+
+    private void Update() {
+        GameObject player = GameManager.Instance().GetPlayer();
+        CarController car_controller = player.GetComponent<CarController>();
+        Vector3 distance = (player.transform.position) - transform.position;
+        bool isNearPlayer = (distance.magnitude < 30);
+
+        if (car_controller.getFearStatus() && !fearStatus && isNearPlayer) {
+            //fearStatus = true;
+            //agent.speed = 5 * agent.speed;
+        }
+        else if (!car_controller.getFearStatus() && fearStatus)
+        {
+            //fearStatus = false;
+            //agent.speed = agent.speed / 5;
+        }
+
+        //if ((agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance < 1) || agent.pathStatus == NavMeshPathStatus.PathInvalid) {
+        //    UpdateTarget();
+        //}
+
+        //animator.SetBool("walking", agent.velocity.magnitude > 0.01);
+    }
+
+    private void UpdateTarget() {
+        currentID++;
+        if (currentID == nbPoints)
+            currentID = 0;
+        currentTarget = targets[currentID];
+        //agent.SetDestination(currentTarget);
+    }
+}
