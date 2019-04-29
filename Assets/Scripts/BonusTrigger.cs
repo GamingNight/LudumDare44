23b1c88f172604b0c8fdd355fc2f8c44bcb16aaf
GamingNight <34170706+GamingNight@users.noreply.github.com>
2019-04-29 23:50:21 +0200

    bonus cartons

---
+++ Assets/Scripts/BonusTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BonusTrigger : MonoBehaviour {
+
+    public int nbPoints;
+    public bool destroyAfterScoring;
+
+    public void OnTriggerEnter(Collider other) {
+
+        GameObject player = GameManager.Instance().GetPlayer();
+        if (other.gameObject == player) {
+            player.GetComponent<StreamViewManager>().UpdateStreamPoints(30);
+            Destroy(gameObject);
+        }
+    }
+}
